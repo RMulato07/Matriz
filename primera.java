da0b60a87b1ae2bc6033300b72cac383ff8b6af3
Ruan Mulato <102757617+RMulato07@users.noreply.github.com>
2023-03-12 22:14:23 -0300

    Add files via upload

---
+++ primera.java
@@ -0,0 +1,23 @@
+package matriz;
+import java.util.Scanner;
+public class primera {
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int matriz [][] = new int [3] [4];
+	
+	for(int i=0; i< 3; i++) { //esse laço controla as linhas
+		for(int j=0; j<4; j++) {//esse controla as colunas
+			System.out.println("digite um numero para posição [" + i + "] ["+ j +"]: ");
+			matriz [i] [j] = in.nextInt();
+		}
+	}
+	for(int i=0; i <3; i++) {
+		for(int j=0; j<4; j++) {
+			System.out.print(matriz [i] [j]+" ");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
